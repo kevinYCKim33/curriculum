d1c0bd5eb7394ed358fb5ed4fc9c550346e0305e
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-11 19:30:24 -0800

    deleted unnecessary case of num === 2

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -7,7 +7,6 @@
 
  const solution = (num, counter=2) => {
    if (num <= 1) { return false }
-   if (num === 2) { return true }
    if (num === counter) { return true }
    if (num % counter === 0) { return false }
    return solution(num, counter + 1);
